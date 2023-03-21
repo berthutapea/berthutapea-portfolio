f99ba23716978fe0e762c0505ba0b561f31ad556
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-03-21 23:38:46 +0700

    Refactor

--- src/assets/blogs.js
+++ src/assets/blogs.js
@@ -1,18 +1,18 @@
-const blogs = [
-  {
-    _id: 1,
-    title: "Basics of Redux & React Redux",
-    path: "basics-of-redux-react-redux",
-    date: "21 Oct 2022",
-    img: "https://res.cloudinary.com/itsproali/image/upload/v1667563043/Portfolio/redux_g8mpif.jpg",
-    medium_link:
-      "https://medium.com/@itsproali/basics-of-redux-react-redux-b98112b88991",
-    linkedin_link:
-      "https://www.linkedin.com/posts/itsproali_basics-of-redux-react-redux-activity-6960961422314942464-HU4t",
-    tags: ["redux", "react-redux", "basic-redux"],
-    description:
-      "Redux is a predictable, open-source state container for javascript applications. Redux makes it more accessible to manage states in a big project and makes the application faster than expected. \n Well, A large application has many kinds of states, which connect one component to other components. It is very complicated to manage and handle states without using the redux. In redux, a state can be used from any parent or child component. \n Let’s introduce the principal workflow of redux. \n Action is the very first step of redux. Action is a function, which returns an Object. Most of the time it has two data. These are “type” and “payload”. Type declare what will do the state and payload contain the data which we want to add or manage. \n Reducer is the second step of working with redux. Reducer is also a function. The reducer has two arguments, “state” and “action”. A state is nothing other than it takes an initial state. And the action takes the action which we previously created. In the inner reducer function, it returns an order by checking the condition of the type of action. \n Store is the global state of an application. It is stored in an Object tree with a single store. A state can be used for any component from the store. To do that App.js should be wrapped with a “Provider” which passes the store. \n Dispatch is a way to use the states in our application. There is a hook called “useDispatch” which comes from “React Redux”. We would use this to call for action and forward the state management process. \n Thank you so much for taking your time. I hope you have gained a piece of basic knowledge about redux and how it works.",
-  },
-];
+// const blogs = [
+//   {
+//     _id: 1,
+//     title: "Basics of Redux & React Redux",
+//     path: "basics-of-redux-react-redux",
+//     date: "21 Oct 2022",
+//     img: "https://res.cloudinary.com/itsproali/image/upload/v1667563043/Portfolio/redux_g8mpif.jpg",
+//     medium_link:
+//       "https://medium.com/@itsproali/basics-of-redux-react-redux-b98112b88991",
+//     linkedin_link:
+//       "https://www.linkedin.com/posts/itsproali_basics-of-redux-react-redux-activity-6960961422314942464-HU4t",
+//     tags: ["redux", "react-redux", "basic-redux"],
+//     description:
+//       "Redux is a predictable, open-source state container for javascript applications. Redux makes it more accessible to manage states in a big project and makes the application faster than expected. \n Well, A large application has many kinds of states, which connect one component to other components. It is very complicated to manage and handle states without using the redux. In redux, a state can be used from any parent or child component. \n Let’s introduce the principal workflow of redux. \n Action is the very first step of redux. Action is a function, which returns an Object. Most of the time it has two data. These are “type” and “payload”. Type declare what will do the state and payload contain the data which we want to add or manage. \n Reducer is the second step of working with redux. Reducer is also a function. The reducer has two arguments, “state” and “action”. A state is nothing other than it takes an initial state. And the action takes the action which we previously created. In the inner reducer function, it returns an order by checking the condition of the type of action. \n Store is the global state of an application. It is stored in an Object tree with a single store. A state can be used for any component from the store. To do that App.js should be wrapped with a “Provider” which passes the store. \n Dispatch is a way to use the states in our application. There is a hook called “useDispatch” which comes from “React Redux”. We would use this to call for action and forward the state management process. \n Thank you so much for taking your time. I hope you have gained a piece of basic knowledge about redux and how it works.",
+//   },
+// ];
 
-export default blogs;
+// export default blogs;
