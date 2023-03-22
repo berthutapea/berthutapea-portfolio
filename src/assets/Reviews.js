8c6ba64109a4812c081530e7da2569cdfe612e56
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-03-22 15:03:34 +0700

    Refactor

--- src/assets/Reviews.js
+++ src/assets/Reviews.js
@@ -1,36 +1,36 @@
 const Reviews = [
   {
     id: 1,
-    name: "John Smith",
-    bio: "MERN-stack Developer",
-    img: "https://www.mecgale.com/wp-content/uploads/2017/08/dummy-profile.png",
+    name: "Aldi Ramdani",
+    bio: "Mobile Developer",
+    img: "https://i.ibb.co/3M64m0f/aldi.jpg",
     description:
       "I am very impressed to work with him. He is a very good and skilled person. I want to work with him again.",
     rating: 5.0,
   },
   {
     id: 2,
-    name: "Sheikh Siam",
+    name: "Ilham Wahyu Budiaji",
     bio: "Front-end Developer",
-    img: "https://i.ibb.co/QphMstM/siam.jpg",
+    img: "https://i.ibb.co/dKYnQM8/ilham.jpg",
     description:
       "He is my partner. We work together. When I fall into an error or any other problem he support me always.",
     rating: 5.0,
   },
   {
     id: 3,
-    name: "Akram Sakib",
-    bio: "Full-stack Developer",
-    img: "https://i.ibb.co/YRJ02j3/sakib.jpg",
+    name: "Dadang Darsono",
+    bio: "CEO MSN Production",
+    img: "https://i.ibb.co/yPSV6Fv/dadang.jpg",
     description:
       "I was give him to make an webpage. He made me an awesome website. I recommend himself and wanna work with him again.",
     rating: 5.0,
   },
   {
     id: 4,
-    name: "Nasimur Rahman",
-    bio: "Jr. Web Developer",
-    img: "https://i.ibb.co/YWLFHDj/nasim.jpg",
+    name: "Raden Amal Prayogo",
+    bio: "Full Stack Developer",
+    img: "https://i.ibb.co/chG1v0f/Amal.jpg",
     description:
       "I was finding someone to help me to build my Grocery website for my store. He made me an E-commerce Store. I am satisfied with his work.",
     rating: 5.0,
