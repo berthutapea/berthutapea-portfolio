ef86154324b96c79708d48168b52169f9f465eac
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-05-03 05:54:45 +0700

    Refactor

--- src/Utils/Items.js
+++ src/Utils/Items.js
@@ -89,7 +89,7 @@ const Items = [
     description:
       "Fullstack open source blogging application made with MongoDB, Express, React & Nodejs (MERN)",
     category: "personal",
-    liveLink: "http://localhost:3000/mern-blog",
+    liveLink: "https://berthutapea.vercel.app/mern-blog",
     codeLink: "https://github.com/berthutapea/mern-blog-v2",
   },
   {
