ec6a8dbe1568477113dad6f6a002b37ef9c529bd
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-05-02 21:56:21 +0700

    Refactor

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -14,7 +14,7 @@ const About = () => {
     <div className="parent pt-16 my-16">
       <div className="">
         <motion.div
-          className="mb-12"
+          className="mb-10"
           initial={{ y: -200, opacity: 0 }}
           animate={{
             y: 0,
@@ -28,7 +28,7 @@ const About = () => {
           </h1>
           <BottomLine />
         </motion.div>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-20">
           <motion.div
             initial={{ x: -200, opacity: 0 }}
             animate={{
@@ -38,10 +38,11 @@ const About = () => {
             }}
           >
             <img
-              src="https://i.ibb.co/TrfBP9b/Gilbert-Hutapea.png"
+              src="https://res.cloudinary.com/du541igfh/image/upload/v1683035791/Blog%20Portfolio%20Gilbert/Project/berthutapea.com/GilbertHutapea_qjy2gq.png"
               alt="Gilbert Hutapea"
-              className="mx-auto"
+              class="p-12 w-70 h-70 transform translate-y-[-12%]"
             />
+
           </motion.div>
           <motion.div
             className=""
@@ -52,10 +53,10 @@ const About = () => {
               transition: { duration: 1, delay: 1.25 },
             }}
           >
-            <h1 className="text-4xl font-semibold mb-0">Gilbert Hutapea</h1>
+            <h1 class="text-4xl font-semibold mb-4 text-center translate-y-[-380%] sm:translate-y-[-0%] sm:text-3xl sm:mb-2 md:text-left">Gilbert Hutapea</h1>
             <div className="my-8">
               <TypeAnimation
-                className="text-2xl text-primary font-bold"
+                className="text-2xl text-primary font-bold text-center translate-y-[-480%] sm:translate-y-[-0%]  sm:text-2xl sm:mb-2 md:text-left"
                 cursor={true}
                 sequence={[
                   "A Front-end Developer",
@@ -69,41 +70,44 @@ const About = () => {
                 repeat={Infinity}
               />
             </div>
-            <p className="text-neutral font-medium">
+            <p className="text-neutral font-medium text-center translate-y-[-120%] sm:translate-y-[-0%] sm:mb-2 md:text-left">
               Dedicated Front-end developer. Capable to solve working problems.
               Passionate about learning & development to reach the target. Eager
               to tackle more complex problems and continue to find ways to
               maximize user efficiency.
             </p>
             <br />
-            <p className="text-neutral font-medium">
+            <p className="text-neutral font-medium text-center translate-y-[-180%] sm:translate-y-[-0%] sm:mb-2 md:text-left">
               My next mission is how to become a Mern stack Developer. I would
               love to build some Giant Website which will shine myself.
             </p>
-            <div className="grid grid-cols-1 lg:grid-cols-2 gap-x-12 gap-y-4 mt-4">
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-x-12 gap-y-4 mt-4 text-center translate-y-[-45%] sm:translate-y-[-0%] sm:mb-2 md:text-left ">
               <h2 className="font-medium">
-                <span className="mr-4 text-primary">Name : </span>Gilbert Hutapea
+                <span className="mr-2 text-primary">Name : </span>Gilbert Hutapea
               </h2>
               <h2 className="font-medium">
-                <span className="mr-4 text-primary">Phone : </span>+62 823 6148 4992
+                <span className="mr-2 text-primary">Phone : </span>+62 823 6148 4992
               </h2>
               <h2 className="font-medium">
-                <span className="mr-4 text-primary">Email : </span>
+                <span className="mr-2 text-primary">Email : </span>
                 berthutapea@gmail.com
               </h2>
               <h2 className="font-medium">
-                <span className="mr-4 text-primary">Address : </span>Karawang, Jawa Barat, Indonesia
+                <span className="mr-2 text-primary">Address : </span>Karawang, Jawa Barat, Indonesia
               </h2>
               <a
                 // href="https://drive.google.com/file/d/1qVQ1OLO-5RxHy1rwbe5Ib9igCTfeO99f/view?usp=sharing"
                 target="blank"
               >
-                <button className="primary-button">
-                  <span>My Resume</span>
-                  <span>
-                    <FaDownload />
-                  </span>
-                </button>
+                <div class="flex justify-center md:justify-start translate-y-[60%] sm:translate-y-[-0%]">
+                  <button class="primary-button">
+                    <span>My Resume</span>
+                    <span>
+                      <FaDownload />
+                    </span>
+                  </button>
+                </div>
+
               </a>
             </div>
           </motion.div>
