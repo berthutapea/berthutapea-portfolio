cd4e08bc79c15602f85ee15ca21126810879c252
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-05-02 22:43:29 +0700

    Refactor

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -39,10 +39,10 @@ const Contact = () => {
     e.preventDefault();
     emailjs
       .sendForm(
-        "itsproali_portfolio",
-        "itsproali_portfolio",
+        "service_6xnj05v",
+        "template_exk29f8",
         form.current,
-        "H-ispiDvwdbG_76iq"
+        "kLfLk-o6LKj-L9c77"
       )
       .then(
         (result) => {
